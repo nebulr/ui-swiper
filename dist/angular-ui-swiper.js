511ae0f261ed9fe361f2aa8198950b2098656d92
James A. Meyer <james.m@seventablets.com>
2017-04-24 10:36:01 -0500

    Making a new production buildi as per new pull request

--- dist/angular-ui-swiper.js
+++ dist/angular-ui-swiper.js
@@ -3830,12 +3830,12 @@ function SwiperDirective (Swiper, $rootScope, $timeout) {
       $attribute.prevButton = '.' + $scope.uuid + ' .swiper-button-prev';
 
       $timeout(function() {
+        ($attribute && $attribute.speed) && ($attribute.speed = parseInt($attribute.speed));
         $scope.instance = new Swiper ('.' + $scope.uuid, $attribute);
       });
 
       $rootScope.$on($scope.uuid, function() {
         $timeout(function() {
-	  ($attribute && $attribute.speed) && ($attribute.speed = parseInt($attribute.speed));
           $scope.instance = new Swiper ('.' + $scope.uuid, $attribute);
         });
       });
